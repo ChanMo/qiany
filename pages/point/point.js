59c3b6a427df957604590c533ea76e6b188a2928
MacBook <chen@MacBook-Air.local>
2018-11-06 20:28:09 +0800

    update

--- pages/point/point.js
+++ pages/point/point.js
@@ -1,1 +1,13 @@
-Page({})
+const api = require('../../api')
+const app = getApp()
+
+Page({
+  data: {
+    active: 'people'
+  },
+  onLoad: function() {
+  },
+  setActive: function(e) {
+    this.setData({active:e.currentTarget.dataset.value})
+  }
+})
