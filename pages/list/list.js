4b701cec80fc097bf941de17de334c1b58891c58
MacBook <chen@MacBook-Air.local>
2018-11-20 22:41:54 +0800

    rename to list

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,51 +1,48 @@
-const api = require('../../api')
-const app = getApp()
+const api = require("../../api")
 
 Page({
   data: {
-    active: 0,
-    category: [],
-    commodities: []
+    sort: 'all',
+    category: 0,
+    search: null,
+    data: []
   },
   onLoad: function(options) {
-    this.setData({active: options.category})
-    this._fetchCategory()
+    if(options.category) {
+      this.setData({category: options.category})
+    }
+    if(options.search) {
+      this.setData({search: options.search})
+    }
+    this._fetchData()
   },
-  _fetchCategory: function() {
-    wx.showLoading()
-    let self = this
-    wx.request({url: api.category, success: function(res) {
-      if(res.data.code > 0) {
-        let category = res.data.data.cate_list
-        self.setData({
-          category: category
-        })
-        self._fetchData()
-      }
-    }})
+  onPullDownRefresh: function() {
+    this._fetchData()
+    wx.stopPullDownRefresh()
   },
-  setActive: function(e) {
-    this.setData({active:e.currentTarget.dataset.value})
+  // 设置排序方式
+  setSort: function(e) {
+    const value = e.currentTarget.dataset.value
+    this.setData({sort: value})
     this._fetchData()
   },
   // 获取数据
   _fetchData: function() {
-    wx.showLoading()
     let self = this
     let url = api.commodities
     wx.request({
       url:url,
-      data: {'category':self.data.active},
+      data: {
+        category_id: this.data.category,
+        sortType: this.data.sort,
+        sortPrice: 'true',
+        search: this.data.search
+      },
       method: 'POST',
       success:function(res){
-        if(res.data.code > 0) {
-          self.setData({commodities:res.data.data.data})
-        }
-      },
-      complete: function() {
-        wx.hideLoading()
+      if(res.data.code > 0) {
+        self.setData({data:res.data.data.data})
       }
-    })
+    }})
   }
-
 })
