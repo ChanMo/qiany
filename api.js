1f5691adb92aea18aaf8a2079c05c6a06a94d4c8
MacBook <chen@MacBook-Air.local>
2018-11-20 22:39:35 +0800

    add pages

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const domain = "http://wx.lcpxj.com/v1/"
+const domain = "https://wx.lcpxj.com/v1/"
 const api = {
   "login": domain + "user/login",
   "sync": domain + "user/userInfo",
@@ -12,10 +12,16 @@ const api = {
   "page": domain + "content/index",
   "addToCart": domain + "cart/add",
   "cart": domain + "cart/lists",
+  "decreaseFromCart": domain + "cart/sub",
+  "deleteFromCart": domain + "cart/delete",
   "buy": domain + "order/cart",
   "pay": domain + "user.order/pay",
   "qrcode": domain + "user/QrCode",
   "orderList": domain + "user.order/lists",
-  "order": domain + "user.order/detail"
+  "order": domain + "user.order/detail",
+  "cancel": domain + "user.order/cancel",
+  "refund": domain + "user.order/refund",
+  "confirm": domain + "user.order/receipt",
+  "delivery": domain + "user.order/express",
 }
 module.exports = api
