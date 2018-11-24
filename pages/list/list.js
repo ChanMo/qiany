1eb6906c3ab09c6de8c6a5a5e3026118ba74e37c
MacBook <chen@MacBook-Air.local>
2018-11-24 21:08:15 +0800

    add price sort

--- pages/list/list.js
+++ pages/list/list.js
@@ -2,11 +2,13 @@ const api = require("../../api")
 
 Page({
   data: {
-    sort: 'all',
-    category: 0,
-    search: null,
-    data: []
+    sort: 'all', // 排序方式
+    asc: true, // 价格正序
+    category: 0, // 分类
+    search: null, // 搜索内容
+    data: [] // 数据
   },
+
   onLoad: function(options) {
     if(options.category) {
       this.setData({category: options.category})
@@ -16,17 +18,34 @@ Page({
     }
     this._fetchData()
   },
+
+  /**
+   * 下拉刷新
+   */
   onPullDownRefresh: function() {
     this._fetchData()
     wx.stopPullDownRefresh()
   },
-  // 设置排序方式
+
+  /*
+   * 设置排序方式
+   */
   setSort: function(e) {
     const value = e.currentTarget.dataset.value
+
+    // 如果是价格
+    if(this.data.sort == 'price' && value == 'price') {
+      let asc = e.currentTarget.dataset.asc == 'true'
+      this.setData({asc: !asc})
+    }
+
     this.setData({sort: value})
     this._fetchData()
   },
-  // 获取数据
+
+  /**
+   * 获取商品列表
+   */
   _fetchData: function() {
     let self = this
     let url = api.commodities
@@ -35,7 +54,7 @@ Page({
       data: {
         category_id: this.data.category,
         sortType: this.data.sort,
-        sortPrice: 'true',
+        sortPrice: this.data.asc,
         search: this.data.search
       },
       method: 'POST',
