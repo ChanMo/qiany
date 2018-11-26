4a51d9bb475d1421974ffcfb0672c1511d40e7ce
MacBook <chen@MacBook-Air.local>
2018-11-26 21:20:15 +0800

    use api

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,10 +1,31 @@
+const api = require('../../api')
+const app = getApp()
+
 Page({
   data: {
-    search: null
+    search: null,
+    keywords: [] // 热门搜索
   },
+
   onLoad: function() {
+    this._fetchHotSearch()
   },
-  // set search value
+
+  /**
+   * 获取热门搜索
+   */
+  _fetchHotSearch: function() {
+    const self = this
+    const url = api.search
+    wx.request({
+      url: url,
+      success: (res)=>self.setData({keywords:res.data.data})
+    })
+  },
+
+  /**
+   * 设置搜索词
+   */
   setValue: function(e) {
     let value = e.detail.value
     wx.navigateTo({url: '/pages/list/list?search='+value})
